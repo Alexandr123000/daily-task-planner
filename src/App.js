11ac7eaba2b09585b2eeed3613faad4a4588f79e
Alexandr123000 <alexandrt10000@gmail.com>
2026-08-18 19:51:02 +0300

    WIP: development process

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button, ButtonGroup } from '@chakra-ui/react';
+import { DatePicker } from "@chakra-ui/react"
+import { today, getLocalTimeZone} from "@internationalized/date"
+import { useState } from "react"
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          daily task planner
-        </p>
         <ButtonGroup isAttached float={"left"}>
         <Button fontSize={"11px"} bg={"red"} h={"30px"} w={"70px"} ml={"2%"} mr={"1%"}>BTN</Button>
         <Button fontSize={"11px"} bg={"red"} h={"30px"} w={"70px"} ml={"4%"} mr={"1%"}>BTN</Button>
         <Button fontSize={"11px"} bg={"red"} h={"30px"} w={"70px"} ml={"4%"} mr={"1%"}>BTN</Button>
         <Button fontSize={"11px"} bg={"red"} h={"30px"} w={"70px"} ml={"4%"} mr={"1%"}>BTN</Button>
         </ButtonGroup>
-      {/*}        <Breadcrumb>
-      <BreadcrumbItem>
-      <BreadcrumbLink>nnn</BreadcrumbLink>
-      </BreadcrumbItem>
-      <BreadcrumbItem>
-      <BreadcrumbLink>nnn</BreadcrumbLink>
-      </BreadcrumbItem>
-            <BreadcrumbItem>
-      <BreadcrumbLink>nnn</BreadcrumbLink>
-      </BreadcrumbItem>
-      
-      </Breadcrumb>*/}
+        <p>
+          Daily Task Planner
+        </p>
+
       </header>
 
     </div>
   );
 }
+
+
 
 export default App;
